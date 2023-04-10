e2ee116873a2f7ce30f3037cd2c54f1d5127092e
hbapte <ijbapte@gmail.com>
2023-04-10 00:13:46 +0000

    markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""Module for converting markdown files to html files"""
-from sys import argv, exit
+"""
+markdown2html - converts a markdown file to HTML
+"""
 
+import sys
+import os
 
 if __name__ == "__main__":
-    """Function for converting"""
-    if len(argv) < 3:
-        exit("Usage: ./markdown2html.py README.md README.html")
-    try:
-        with open(argv[1], "r") as markdown_file:
-            with open(argv[2], "w") as html_file:
-                for line in markdown_file:
-                    if "###### " in line:
-                        line = line.replace("###### ", "<h6>")
-                        line = line.replace("\n", "</h6>\n")
-                    elif "##### " in line:
-                        line = line.replace("##### ", "<h5>")
-                        line = line.replace("\n", "</h5>\n")
-                    elif "#### " in line:
-                        line = line.replace("#### ", "<h4>")
-                        line = line.replace("\n", "</h4>\n")
-                    elif "### " in line:
-                        line = line.replace("### ", "<h3>")
-                        line = line.replace("\n", "</h3>\n")
-                    elif "## " in line:
-                        line = line.replace("## ", "<h2>")
-                        line = line.replace("\n", "</h2>\n")
-                    elif "# " in line:
-                        line = line.replace("# ", "<h1>")
-                        line = line.replace("\n", "</h1>\n")
-                    html_file.write(line)
-    except:
-        exit("Missing {}".format(argv[1]))
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.isfile(md_file):
+        sys.stderr.write("Missing {}\n".format(md_file))
+        sys.exit(1)
+
+    sys.exit(0)
+
